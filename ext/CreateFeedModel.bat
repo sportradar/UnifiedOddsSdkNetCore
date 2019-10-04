636e0b4e1c314dc41429ee88e8cc4db2717c4f54
David Hrovat <d.hrovat@sportradar.com>
2019-10-04 15:26:44 +0200

    copied ext files

---
+++ ext/CreateFeedModel.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM set DevEnvDir="C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools"
+set DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools"
+set DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools"
+set DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools"
+call %DevEnvDir%\..\Tools\vsvars32.bat
+REM REM REM KILL KILL KILL
+
+@xsd.exe /c /l:C# UnifiedFeedAMQP.xsd /n:Sportradar.OddsFeed.SDK.Messages.Internal.Feed
+REM @move /y UnifiedFeed.cs.new UnifiedFeed.cs
---
+++ ext/CreateProbabilitiesModel.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM set DevEnvDir="C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools"
+set DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools"
+set DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools"
+set DevEnvDir="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools"
+call %DevEnvDir%\..\Tools\vsvars32.bat
+REM REM REM KILL KILL KILL
+
+@xsd.exe /c /l:C# /n:Sportradar.OddsFeed.SDK.Messages UnifiedFeedCashoutOdds.xsd
