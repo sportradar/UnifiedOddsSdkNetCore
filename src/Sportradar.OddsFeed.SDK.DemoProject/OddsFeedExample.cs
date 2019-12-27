5c5d42a9be93866b3e303d3df8db8467243c5583
David Hrovat <d.hrovat@sportradar.com>
2019-12-27 17:24:12 +0100

    updated DemoProject

--- src/Sportradar.OddsFeed.SDK.DemoProject/OddsFeedExample.cs
+++ src/Sportradar.OddsFeed.SDK.DemoProject/OddsFeedExample.cs
@@ -46,18 +46,18 @@ namespace Sportradar.OddsFeed.SDK.DemoProject
         private static void DoExampleSelection()
         {
             Console.WriteLine(string.Empty);
-            Console.WriteLine(" Select which example you want to run:");
-            Console.WriteLine(" 1 - Basic \t\t\t(single session, generic dispatcher, basic output)");
-            Console.WriteLine(" 2 - Multi-Session \t\t(has Low and High priority sessions)");
-            Console.WriteLine(" 3 - Use specific dispatchers \t(uses specific message dispatchers)");
-            Console.WriteLine(" 4 - Display Market info \t(generates and displays market/outcomes names)");
-            Console.WriteLine(" 5 - Display SportEvent info \t(displays info about sport events)");
-            Console.WriteLine(" 6 - All together \t\t(includes all options above)");
-            Console.WriteLine(" 7 - Extra: Market Mappings \t(displays market mappings related to sport events)");
-            Console.WriteLine(" 8 - Extra: Replay Server \t(how to interact with xReplay Server)");
+            Console.WriteLine("  Select which example you want to run:");
+            Console.WriteLine("  1 - Basic \t\t\t(single session, generic dispatcher, basic output)");
+            Console.WriteLine("  2 - Multi-Session \t\t(has Low and High priority sessions)");
+            Console.WriteLine("  3 - Use specific dispatchers \t(uses specific message dispatchers)");
+            Console.WriteLine("  4 - Display Market info \t(generates and displays market/outcomes names)");
+            Console.WriteLine("  5 - Display SportEvent info \t(displays info about sport events)");
+            Console.WriteLine("  6 - All together \t\t(includes all options above)");
+            Console.WriteLine("  7 - Extra: Market Mappings \t(displays market mappings related to sport events)");
+            Console.WriteLine("  8 - Extra: Replay Server \t(how to interact with xReplay Server)");
             Console.WriteLine("  9 - Extra: Export/import \t(how to export/import current cache state)");
             Console.WriteLine(" 10 - Extra: Advanced setup \t(single session with multi-threaded message parsing)");
-            Console.Write(" Enter number: ");
+            Console.Write("  Enter number: ");
             var k = Console.ReadLine();
 
             var defaultLocale = Feed.GetConfigurationBuilder().SetAccessTokenFromConfigFile().SelectIntegration().LoadFromConfigFile().Build().DefaultLocale;
@@ -72,51 +72,51 @@ namespace Sportradar.OddsFeed.SDK.DemoProject
                         new Basic(_log).Run(MessageInterest.AllMessages);
                         break;
                     }
-                //    case "2":
-                //    {
-                //        new MultiSession(_log).Run();
-                //        break;
-                //    }
-                //    case "3":
-                //    {
-                //        new SpecificDispatchers(_log).Run(MessageInterest.AllMessages);
-                //        break;
-                //    }
-                //    case "4":
-                //    {
-                //        new ShowMarketNames(_log).Run(MessageInterest.AllMessages, defaultLocale);
-                //        break;
-                //    }
-                //    case "5":
-                //    {
-                //        new ShowEventInfo(_log).Run(MessageInterest.AllMessages, defaultLocale);
-                //        break;
-                //    }
-                //    case "6":
-                //    {
-                //        new CompleteInfo(_log).Run(MessageInterest.AllMessages, defaultLocale);
-                //        break;
-                //    }
-                //    case "7":
-                //    {
-                //        new ShowMarketMappings(_log).Run(MessageInterest.AllMessages, defaultLocale);
-                //        break;
-                //    }
-                //    case "8":
-                //    {
-                //        new ReplayServer(_log).Run(MessageInterest.AllMessages);
-                //        break;
-                //    }
-                //    case "9":
-                //    {
-                //        new CacheExportImport(_log).Run(MessageInterest.AllMessages);
-                //        break;
-                //    }
-                //    case "10":
-                //    {
-                //        new MultiThreaded(_log).Run(MessageInterest.AllMessages);
-                //        break;
-                //    }
+                case "2":
+                    {
+                        new MultiSession(_log).Run();
+                        break;
+                    }
+                case "3":
+                    {
+                        new SpecificDispatchers(_log).Run(MessageInterest.AllMessages);
+                        break;
+                    }
+                case "4":
+                    {
+                        new ShowMarketNames(_log).Run(MessageInterest.AllMessages, defaultLocale);
+                        break;
+                    }
+                case "5":
+                    {
+                        new ShowEventInfo(_log).Run(MessageInterest.AllMessages, defaultLocale);
+                        break;
+                    }
+                case "6":
+                    {
+                        new CompleteInfo(_log).Run(MessageInterest.AllMessages, defaultLocale);
+                        break;
+                    }
+                case "7":
+                    {
+                        new ShowMarketMappings(_log).Run(MessageInterest.AllMessages, defaultLocale);
+                        break;
+                    }
+                case "8":
+                    {
+                        new ReplayServer(_log).Run(MessageInterest.AllMessages);
+                        break;
+                    }
+                case "9":
+                    {
+                        new CacheExportImport(_log).Run(MessageInterest.AllMessages);
+                        break;
+                    }
+                case "10":
+                    {
+                        new MultiThreaded(_log).Run(MessageInterest.AllMessages);
+                        break;
+                    }
                 default:
                     {
                         DoExampleSelection();
